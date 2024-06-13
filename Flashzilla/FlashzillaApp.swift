5cfe1b39fd29ac510c6e2847a58c05473a806e09
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2024-06-13 13:12:19 +0545

    initial commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Roshan Bade on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
